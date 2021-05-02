8ca4c0e7ccd0b204d3c2a72d1bb9088ff56efe8d
CDjepeno <djepeno.christopher@gmail.com>
2021-05-02 23:54:20 +0200

    Make connection in mongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,20 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 
 dotenv.config()
 
 const PORT = process.env.PORT || 5000
 const app = express();
+
+/**
+ * Connect MongoDB
+ */
+mongoose.connect(process.env.MONGODB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+})
 
 app.listen(PORT, () => {
     console.log(`Listening at port ${PORT}`)
